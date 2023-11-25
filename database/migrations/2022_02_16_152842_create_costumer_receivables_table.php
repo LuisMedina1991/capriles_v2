16b0dcb6b34c86316c66bfad56dd868747df9f9e
LuisMedina1991 <luchito19medina91@gmail.com>
2023-11-25 01:54:42 -0400

    cambio a campo amount para que acepte mas decimales

--- database/migrations/2022_02_16_152842_create_costumer_receivables_table.php
+++ database/migrations/2022_02_16_152842_create_costumer_receivables_table.php
@@ -15,8 +15,8 @@ class CreateCostumerReceivablesTable extends Migration
     {
         Schema::create('costumer_receivables', function (Blueprint $table) {
             $table->id();
-            $table->text('description',1000);
-            $table->decimal('amount',10,2);
+            $table->text('description');
+            $table->decimal('amount',60,30);
             $table->foreignId('costumer_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
