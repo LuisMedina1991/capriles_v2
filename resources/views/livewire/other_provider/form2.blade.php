e5b54ece7242f4a66ac1bab8b916799e803a5241
LuisMedina1991 <luchito19medina91@gmail.com>
2024-01-08 13:41:05 -0400

    refactorizacion para corregir metodos edit/update

--- resources/views/livewire/other_provider/form2.blade.php
+++ resources/views/livewire/other_provider/form2.blade.php
@@ -9,7 +9,7 @@
         </div>
         <div class="modal-body">
             <div class="row">
-                <div class="col-sm-12 col-md-12">
+                <div class="col-sm-12">
                     <div class="form-group">
                         <label>Referencia de la deuda</label>
                         <input type="text" wire:model.lazy="reference" class="form-control" placeholder="Referencia de la deuda...">
@@ -18,25 +18,7 @@
                         @enderror
                     </div>
                 </div>
-                <div class="col-sm-12 col-md-6">
-                    <div class="form-group">
-                        <label>Saldo actual</label>
-                        <input type="text" wire:model.lazy="amount" class="form-control" placeholder="Monto a cobrar..." disabled>
-                        @error('amount')
-                            <span class="text-danger er">{{ $message }}</span>
-                        @enderror
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-6">
-                    <div class="form-group">
-                        <label>Monto a pagar</label>
-                        <input type="text" wire:model.lazy="amount_2" class="form-control" placeholder="0.00">
-                        @error('amount_2')
-                            <span class="text-danger er">{{ $message }}</span>
-                        @enderror
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-12">
+                <div class="col-sm-12">
                     <div class="form-group">
                         <label>Descripcion de la deuda</label>
                         <textarea wire:model.lazy="description" class="form-control" placeholder="Detalles de la deuda..." cols="30" rows="3"></textarea>
@@ -45,15 +27,52 @@
                         @enderror
                     </div>
                 </div>
-                <div class="col-sm-12 col-md-12">
+                <div class="col-sm-12 col-md-6">
                     <div class="form-group">
-                        <label>Detalles</label>
-                        <textarea wire:model.lazy="description_2" class="form-control" placeholder="Detalles del movimiento..." cols="30" rows="3"></textarea>
-                        @error('description_2')
+                        <label>Saldo actual</label>
+                        <input type="text" wire:model="amount" class="form-control" placeholder="Monto a cobrar..." disabled>
+                        @error('amount')
                             <span class="text-danger er">{{ $message }}</span>
                         @enderror
                     </div>
                 </div>
+                <div class="col-sm-12 col-md-6">
+                    <div class="form-group">
+                        <label>Accion</label>
+                        <select wire:model="action" class="form-control">
+                            <option value="Elegir">Elegir</option>
+                            <option value="egreso">Egreso</option>
+                            <option value="edicion">Solo Edicion</option>
+                        </select>
+                        @error('action')
+                            <span class="text-danger er">{{ $message }}</span>
+                        @enderror
+                    </div>
+                </div>
+
+                @if($action == 'egreso')
+
+                    <div class="col-sm-12">
+                        <div class="form-group">
+                            <label>Detalles</label>
+                            <textarea wire:model.lazy="discharge_description" class="form-control" placeholder="Detalles del pago..." cols="30" rows="3"></textarea>
+                            @error('discharge_description')
+                                <span class="text-danger er">{{ $message }}</span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+                        <div class="form-group">
+                            <label>Monto a pagar</label>
+                            <input type="text" wire:model.lazy="discharge_amount" class="form-control" placeholder="0.00">
+                            @error('discharge_amount')
+                                <span class="text-danger er">{{ $message }}</span>
+                            @enderror
+                        </div>
+                    </div>
+
+                @endif
+
             </div>
         </div>
         <div class="modal-footer">
