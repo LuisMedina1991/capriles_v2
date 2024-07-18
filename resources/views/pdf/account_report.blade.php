3b66d806c1624fdda3cc1fe02d16c07ce0f4d153
LuisMedina1991 <luchito19medina91@gmail.com>
2024-07-18 14:59:00 -0400

    correccion en encabezado de reporte pdf de movimientos bancarios

--- resources/views/pdf/account_report.blade.php
+++ resources/views/pdf/account_report.blade.php
@@ -9,18 +9,20 @@
     <link rel="stylesheet" href="{{ public_path('css/custom_page.css') }}"> <!--estilos de hoja pdf-->
 </head>
 <body>
-    <section class="header" style="top: -287px;">
-        <table cellpading="0" cellspacing="0" width="100%">
+    <header>
+        <table width="100%">
             <tr>
-                <td colspan="2" class="text-center">
+                <td class="text-center">
                     <span style="font-size: 25px; font-weigth: bold;">Importadora Capriles</span>
                 </td>
             </tr>
             <tr>
-                <td style="vertical-align: top; padding-top: 5px; padding-right: 15px; position: relative" width="30%">
-                    <img src="{{ public_path('assets/img/LOGO3.png') }}" alt="" class="invoice-logo" style="">
+                <td class="text-center">
+                    <span style="font-size: 20px; font-weigth: bold;">Movimientos Bancarios</span>
                 </td>
-                <td width="70%" class="text-left text-company" style="vertical-align: top; padding-top: 10px;">
+            </tr>
+            <tr>
+                <td class="text-center">
                     @if ($reportRange == 0)
                         <span style="font-size: 16px"><strong>Movimientos del Dia</strong></span>
                         <br>
@@ -35,14 +37,14 @@
                 </td>
             </tr>
         </table>
-    </section>
-    <section style="margin-top: -150px">
+    </header>
+    <section>
         <table cellpadding="0" cellspacing="0" class="table-items" width="100%">
             <thead>
                 <tr>
                     <th width="40%">Descripcion</th>
-                    <th width="15%">Monto</th>
                     <th width="15%">Saldo previo</th>
+                    <th width="15%">Monto</th>
                     <th width="15%">Saldo Actual</th>
                     <th width="15%">Fecha</th>
                 </tr>
@@ -51,28 +53,13 @@
                 @foreach ($details as $detail)
                     <tr>
                         <td align="center">{{ $detail->description }}</td>
-                        <td align="center">${{ number_format($detail->amount,2) }}</td>
                         <td align="center">${{ number_format($detail->previus_balance,2) }}</td>
+                        <td align="center">${{ number_format($detail->amount,2) }}</td>
                         <td align="center">${{ number_format($detail->actual_balance,2) }}</td>
                         <td align="center">{{\Carbon\Carbon::parse($detail->created_at)->format('d-m-Y')}}</td>
                     </tr>
                 @endforeach
             </tbody>
-        </table>
-    </section>
-    <section class="footer">
-        <table cellpadding="0" cellspacing="0" class="table-items" width="100%">
-            <tr>
-                <td width="20%">
-                    <span>IMPORTADORA CAPRILES</span>
-                </td>
-                <td width="60%" class="text-center">
-                    importadoracapriles.com.bo
-                </td>
-                <td class="text-center" width="20%">
-                    Pagina <span class="pagenum"></span>
-                </td>
-            </tr>
         </table>
     </section>
 </body>
